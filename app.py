406194c85829d7926f22df8b4e7acc9bdc3fd445
Razzi Abuissa <razzi@abuissa.net>
2025-06-25 00:45:21 +0300

    App using template

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+guests = ['Razzi Abuissa']
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        guests.append(request.form['name'])
+
+    return render_template('home.html', guests=guests)
